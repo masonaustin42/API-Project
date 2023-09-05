2ccea10a4767683cd667d86230a450122b7c727c
masonaustin42 <masonaustin42@gmail.com>
2023-09-05 15:18:30 -0700

    Fix seeder files from demo-bookings to demo-spot-imgs

--- backend/db/seeders/20230905205628-demo-reviews.js
+++ backend/db/seeders/20230905205628-demo-reviews.js
@@ -38,10 +38,14 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews";
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      spotId: {
-        [Op.in]: [1, 2, 3],
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: {
+          [Op.in]: [1, 2, 3],
+        },
       },
-    });
+      {}
+    );
   },
 };
