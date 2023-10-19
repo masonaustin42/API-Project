d11f51d7f7feddff34f1ac49d34a4330797b0cd1
masonaustin42 <masonaustin42@gmail.com>
2023-10-19 14:23:52 -0700

    complete csrf auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,4 +13,35 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
+// Serve React build files in production environment
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
+  // Serve frontend's index.html at root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+
+  // Serve static assets in frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve frontend's index.html file at all other routes that do NOT start with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.json({});
+  });
+}
 module.exports = router;
