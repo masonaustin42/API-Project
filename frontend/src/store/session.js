ac1c81bc8097461e0fc11e8119e7cfe82bd486ad
masonaustin42 <masonaustin42@gmail.com>
2023-10-20 11:00:08 -0700

    Add user login/logout thunks to session.js

---
+++ frontend/src/store/session.js
@@ -0,0 +1,63 @@
+import { csrfFetch } from "./csrf";
+
+const LOGIN = "session/LOGIN";
+const LOGOUT = "session/LOGOUT";
+
+const logIntoState = (user) => {
+  return {
+    type: LOGIN,
+    user,
+  };
+};
+
+const logOutOfState = () => {
+  return {
+    type: LOGOUT,
+  };
+};
+
+export const logIn =
+  ({ credential, password }) =>
+  async (dispatch) => {
+    const res = await csrfFetch("/api/session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        credential,
+        password,
+      }),
+    });
+
+    if (res.ok) {
+      const userResponse = await res.json();
+      dispatch(logIntoState(userResponse.user));
+      return userResponse.user;
+    }
+  };
+
+export const logOut = () => async (dispatch) => {
+  const res = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+
+  if (res.ok) {
+    dispatch(logOutOfState());
+    return await res.json();
+  }
+};
+
+const sessionReducer = (state = { user: null }, action) => {
+  switch (action.type) {
+    case LOGIN:
+      console.log("Hit login case!");
+      return { ...state, user: action.user };
+    case LOGOUT:
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
