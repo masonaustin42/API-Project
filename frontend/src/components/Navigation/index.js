48c6316043dafc0de43779fbf1c017c802b42744
masonaustin42 <masonaustin42@gmail.com>
2023-10-23 15:56:36 -0700

    added styles for navbar and made base style

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,47 +1,18 @@
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-import LoginFormModal from "../LoginFormModal";
-import OpenModalButton from "../OpenModalButton";
-import SignupFormModal from "../SignupFormModal";
-import logo from "../../images/airdnd_logo.png";
+import logo from "../../images/airdnd_logo_full.png";
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
-
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    );
-  }
-
   return (
     <nav className="navbar">
       <div className="logo">
         <NavLink exact to="/">
           <img src={logo} alt="AirDnD Logo" />
-          airdnd
         </NavLink>
       </div>
 
-      {isLoaded && sessionLinks}
+      {isLoaded && <ProfileButton />}
     </nav>
   );
 }
